1bc68795ad66312bb0e7d0177cdeeea9724b4ec8
Seif Elmughrabi <Seif Elmughrabi>
2017-01-28 08:11:02 +0200

    adding tomato.py

---
+++ tomato.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+
+while True:
+	time.sleep(1500)
+	webbrowser.open("https://www.youtube.com/watch?v=1JjKrxjzIZQ", new = 1, autoraise = True)
+	time.sleep(300)
+	webbrowser.open("https://www.youtube.com/watch?v=pzs1MEmDNT8", new = 1, autoraise = True)
+	
